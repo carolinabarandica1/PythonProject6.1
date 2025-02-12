51ef99bd5eee86da28485dc0e4d00007cbd59209
carolinabarandica <carolinabarandica1@gmail.com>
2025-02-12 18:34:45 +0100

    Merge remote-tracking branch 'origin/main'

--- book.py
+++ book.py
@@ -1,3 +1,14 @@
+from enum import unique
+from string import punctuation
+
 import requests
 
 link = "https://gutenberg.org/cache/epub/84/pg84"
+
+result = requests.get(link)
+unique_words = {}
+punctuation = ",.'!?-=()"
+print(result.text)
+
+for line in result.text.splitlines():
+    print()
