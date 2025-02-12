b8654793f45c3da9ac18916bdf83d9153ee25d47
carolinabarandica <carolinabarandica1@gmail.com>
2025-02-12 18:34:57 +0100

    Merge remote-tracking branch 'origin/main'

--- book.py
+++ book.py
@@ -1,14 +1,21 @@
-from enum import unique
-from string import punctuation
-
 import requests
 
-link = "https://gutenberg.org/cache/epub/84/pg84"
+link = "https://www.gutenberg.org/files/84/84-0.txt"
 
 result = requests.get(link)
+text = result.text
 unique_words = {}
 punctuation = ",.'!?-=()"
-print(result.text)
 
-for line in result.text.splitlines():
-    print()
+for line in text.splitlines():
+    for p in punctuation:
+        line = line.replace(p, "")  # Correct placement of .replace()
+
+    words = line.lower().split()
+    for word in words:
+        unique_words[word] = unique_words.get(word, 0) + 1
+
+for word, count in list(unique_words.items())[:10]:
+    print(f"{word}: {count}")
+
+
